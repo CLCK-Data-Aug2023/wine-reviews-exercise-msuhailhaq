5e31ed65e99c954a78a50c968ba4d968a2660efe
Cocanut Suhail <143038533+msuhailhaq@users.noreply.github.com>
2024-01-11 19:39:49 -0500

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,14 +1,12 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('data/winemag-data-130k-v2.csv.zip', compression='zip')
+data = pd.read_csv('data/winemag-data-130k-v2.csv')
+print(data.head())
+print(data.columns)
+mean_points = data.groupby("country")["points"].mean().round(1)
+data.country.value_counts()
+value_counts = data.country.value_counts(ascending=False)
+result = pd.concat([value_counts, mean_points], axis=1, join="inner")
 
-summary_data = df.groupby('country').agg({
-    'points': 'mean',
-    'title': 'count'
-}).reset_index()
-
-summary_data.columns = ['country', 'average_points', 'number_of_reviews']
-summary_data.to_csv('data/reviews-per-country.csv', index=False)
-
-print("Summary data written to reviews-per-country.csv")
-
+print(result)
